178e815bd30b80d5443dc4d63627a025b93aaf56
BUGARJA <bugarinjm.93@gmail.com>
2019-10-20 09:41:15 +0800

    created ParkingLot entity

---
+++ src/main/java/com/thoughtworks/parking_lot/entity/ParkingLot.java
@@ -0,0 +1,45 @@
+package com.thoughtworks.parking_lot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class ParkingLot {
+
+    @Id
+    @NotNull
+    @Column(unique=true,nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "INT(5) CHECK (capacity >= 0)")
+    private Integer capacity;
+
+
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}

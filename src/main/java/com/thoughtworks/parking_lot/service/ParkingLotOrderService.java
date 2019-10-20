bf4a0d4e12ccc668067c46dfc2a34aab600456d1
BUGARJA <bugarinjm.93@gmail.com>
2019-10-20 21:10:53 +0800

    change public variable to private

--- src/main/java/com/thoughtworks/parking_lot/service/ParkingLotOrderService.java
+++ src/main/java/com/thoughtworks/parking_lot/service/ParkingLotOrderService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class ParkingLotOrderService {
 
     private static final String OPEN = "Open";
-    public static final String CLOSE = "Close";
+    private static final String CLOSE = "Close";
 
     @Autowired
     private ParkingLotOrderRepository parkingLotOrderRepo;
